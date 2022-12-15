c6dad614d94f2a2191a33f52a9250b7fc2d9ca04
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2022-12-15 11:52:03 +0200

    client ready

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -2,19 +2,19 @@ import axios from "axios";
 
 export const getVideos = async () => {
   const token = localStorage.getItem("token");
-  const videos = "http://localhost:3999/videos";
+  const videos = "https://shorty-i6gt.onrender.com/videos";
   return await axios.get(videos, { headers: { "x-auth-token": token } });
 };
 
 export const getVideo = async (id) => {
   const token = localStorage.getItem("token");
-  const video = `http://localhost:3999/video/${id}`;
+  const video = `https://shorty-i6gt.onrender.com/video/${id}`;
   return await axios.get(video, { headers: { "x-auth-token": token } });
 };
 
 export const uploadVideo = async (data) => {
   const token = localStorage.getItem("token");
-  const upload = `http://localhost:3999/upload`;
+  const upload = `https://shorty-i6gt.onrender.com/upload`;
   return await axios.post(upload, data, {
     headers: { "x-auth-token": token, "Content-Type": "multipart/form-data" },
   });
@@ -22,55 +22,55 @@ export const uploadVideo = async (data) => {
 
 export const likeVideo = async (id) => {
   const token = localStorage.getItem("token");
-  const like = `http://localhost:3999/like/${id}`;
+  const like = `https://shorty-i6gt.onrender.com/like/${id}`;
   return await axios.post(like, {}, { headers: { "x-auth-token": token } });
 };
 
 export const DislikeVideo = async (id) => {
   const token = localStorage.getItem("token");
-  const like = `http://localhost:3999/dislike/${id}`;
+  const like = `https://shorty-i6gt.onrender.com/dislike/${id}`;
   return await axios.delete(like, { headers: { "x-auth-token": token } });
 };
 
 export const didliked = async (id) => {
   const token = localStorage.getItem("token");
-  const didliked = `http://localhost:3999/didliked/${id}`;
+  const didliked = `https://shorty-i6gt.onrender.com/didliked/${id}`;
   return await axios.get(didliked, { headers: { "x-auth-token": token } });
 };
 
 export const Profile = async (id) => {
   const token = localStorage.getItem("token");
-  const profile = `http://localhost:3999/profile/${id}`;
+  const profile = `https://shorty-i6gt.onrender.com/profile/${id}`;
   return await axios.get(profile, { headers: { "x-auth-token": token } });
 };
 
 export const Follow = async (id) => {
   const token = localStorage.getItem("token");
-  const follow = `http://localhost:3999/follow/${id}`;
+  const follow = `https://shorty-i6gt.onrender.com/follow/${id}`;
   return await axios.post(follow, {}, { headers: { "x-auth-token": token } });
 };
 
 export const UnFollow = async (id) => {
   const token = localStorage.getItem("token");
-  const unfollow = `http://localhost:3999/unFollow/${id}`;
+  const unfollow = `https://shorty-i6gt.onrender.com/unFollow/${id}`;
   return await axios.delete(unfollow, { headers: { "x-auth-token": token } });
 };
 
 export const DidFollow = async (id) => {
   const token = localStorage.getItem("token");
-  const didFollow = `http://localhost:3999/didFollow/${id}`;
+  const didFollow = `https://shorty-i6gt.onrender.com/didFollow/${id}`;
   return await axios.get(didFollow, { headers: { "x-auth-token": token } });
 };
 
 export const GetComments = async (id) => {
   const token = localStorage.getItem("token");
-  const getComments = `http://localhost:3999/getComments/${id}`;
+  const getComments = `https://shorty-i6gt.onrender.com/getComments/${id}`;
   return await axios.get(getComments, { headers: { "x-auth-token": token } });
 };
 
 export const doComment = async (videoId, userId, details) => {
   const token = localStorage.getItem("token");
-  const comment = `http://localhost:3999/comment/${videoId}/${userId}`;
+  const comment = `https://shorty-i6gt.onrender.com/comment/${videoId}/${userId}`;
   return await axios.post(
     comment,
     { details },
@@ -80,7 +80,7 @@ export const doComment = async (videoId, userId, details) => {
 
 export const GetNotifications = async () => {
   const token = localStorage.getItem("token");
-  const GetNotifications = `http://localhost:3999/inbox`;
+  const GetNotifications = `https://shorty-i6gt.onrender.com/inbox`;
   return await axios.get(GetNotifications, {
     headers: { "x-auth-token": token },
   });
@@ -88,7 +88,7 @@ export const GetNotifications = async () => {
 
 export const SearchUsers = async (value) => {
   const token = localStorage.getItem("token");
-  const SearchUsers = `http://localhost:3999/search/users/${value}`;
+  const SearchUsers = `https://shorty-i6gt.onrender.com/search/users/${value}`;
   return await axios.get(SearchUsers, {
     headers: { "x-auth-token": token },
   });
@@ -96,7 +96,7 @@ export const SearchUsers = async (value) => {
 
 export const SearchVideos = async (value) => {
   const token = localStorage.getItem("token");
-  const SearchVideos = `http://localhost:3999/search/videos/${value}`;
+  const SearchVideos = `https://shorty-i6gt.onrender.com/search/videos/${value}`;
   return await axios.get(SearchVideos, {
     headers: { "x-auth-token": token },
   });
@@ -104,7 +104,7 @@ export const SearchVideos = async (value) => {
 
 export const DeleteVideo = async (id) => {
   const token = localStorage.getItem("token");
-  const DeleteVidoe = `http://localhost:3999/delete/${id}`;
+  const DeleteVidoe = `https://shorty-i6gt.onrender.com/delete/${id}`;
   return await axios.delete(DeleteVidoe, {
     headers: { "x-auth-token": token },
   });
@@ -112,7 +112,7 @@ export const DeleteVideo = async (id) => {
 
 export const GetProfileForEdit = async () => {
   const token = localStorage.getItem("token");
-  const GetProfileForEdit = `http://localhost:3999/getProfile/edit`;
+  const GetProfileForEdit = `https://shorty-i6gt.onrender.com/getProfile/edit`;
   return await axios.get(GetProfileForEdit, {
     headers: { "x-auth-token": token },
   });
@@ -120,7 +120,7 @@ export const GetProfileForEdit = async () => {
 
 export const UpdateProfile = async (data) => {
   const token = localStorage.getItem("token");
-  const UpdateProfile = `http://localhost:3999/update`;
+  const UpdateProfile = `https://shorty-i6gt.onrender.com/update`;
   return await axios.put(UpdateProfile, data, {
     headers: { "x-auth-token": token },
   });
@@ -128,7 +128,7 @@ export const UpdateProfile = async (data) => {
 
 export const UpdateProfilePhoto = async (data) => {
   const token = localStorage.getItem("token");
-  const UpdateProfile = `http://localhost:3999/update/photo`;
+  const UpdateProfile = `https://shorty-i6gt.onrender.com/update/photo`;
   return await axios.put(UpdateProfile, data, {
     headers: { "x-auth-token": token, "Content-Type": "multipart/form-data" },
   });
--- client/src/api/userApi.js
+++ client/src/api/userApi.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
 export const LoginApi = async (data) => {
-  const loginApi = "http://localhost:3999/login";
+  const loginApi = "https://shorty-i6gt.onrender.com/login";
   return await axios.post(loginApi, data);
 };
 
 export const RegisterApi = async (data) => {
-  const registerApi = "http://localhost:3999/register";
+  const registerApi = "https://shorty-i6gt.onrender.com/register";
   return await axios.post(registerApi, data);
 };
 
 export const AllowJoin = async () => {
   const token = localStorage.getItem("token");
-  const AllowJoin = "http://localhost:3999/allow";
+  const AllowJoin = "https://shorty-i6gt.onrender.com/allow";
   return await axios.get(AllowJoin, {
     headers: { "x-auth-token": token },
   });
